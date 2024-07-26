c6f575c2b6633363a0d4f47c25da726b40bbeb1e
Surya <suryakant.subudhi@outlook.com>
2024-07-26 21:11:24 +0530

    build: animated hero text

--- slices/HeroSection/index.tsx
+++ slices/HeroSection/index.tsx
@@ -1,6 +1,9 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+"use client";
 
+import { Content, KeyTextField } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
 /**
  * Props for `HeroSection`.
  */
@@ -10,12 +13,77 @@ export type HeroSectionProps = SliceComponentProps<Content.HeroSectionSlice>;
  * Component for "HeroSection" Slices.
  */
 const HeroSection = ({ slice }: HeroSectionProps): JSX.Element => {
+
+  const component = useRef(null);
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      const tl = gsap.timeline();
+
+      tl.fromTo(".name-animation", {
+        x: -100, opacity: 0, rotate: -10
+      },
+      {
+        x: 0, 
+        opacity: 100, 
+        rotate: 0,
+        ease: "elastic.out(1, 0.3)",
+        duration: 1,
+        transformOrigin: "left top",
+        stagger: {
+          each: 0.1,
+          from: "random"
+        }
+      }
+    );
+
+    tl.fromTo(".tag-line", {
+      y: 20, opacity: 0, scale: 1.2,
+    }, {
+      y: 0, 
+      opacity: 1, 
+      scale: 1,
+      delay: 0.5,
+      ease: "elastic.out(1, 0.3)",
+      duration: 1,
+    })
+
+    }, component);
+    return () => ctx.revert();
+  }, []);
+
+  const renderLetters = (name: KeyTextField, key: string) => {
+    if(name) {
+      return name?.split('').map((letter, index) => (
+        <span key={index} className={`name-animation name-animation-${key} inline-block opacity-0`}>
+          {letter}
+        </span>
+      ))
+    }
+  }
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      ref={component}
     >
-      <>{slice.primary.first_name}</>
+      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <div className="col-start-1 md:row-start-1">
+            <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.first_name + " " + slice.primary.last_name}>
+              <span className="block text-zinc-200">
+                {renderLetters(slice.primary.first_name, "first")}
+              </span>
+              <span className="-mt-[.2em] block text-zinc-600">
+                {renderLetters(slice.primary.last_name, "last")}
+              </span>
+            </h1>
+            <span className="tag-line block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">
+                {slice.primary.tag_line}
+            </span>
+        </div>
+
+      </div>
     </section>
   );
 };
